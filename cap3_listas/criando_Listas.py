3bf959d395a389832e8dfadff82a0577a496ca2a
GiovanneBYTE <giovanne_25@hotmail.com>
2022-09-26 21:43:16 -0300

    list ex

---
+++ cap3_listas/criando_Listas.py
@@ -0,0 +1,9 @@
+#lista preenchida estaticamente
+lista_estatica = ["xpto", True]
+
+#LISTA PREENCHIDA DINAMICAMENTE
+lista_dinamica = [input("Digite o usuario: "), bool(int(input("Está logado? ")))]
+
+#LISTA VAZIA
+lista_vazia=[]
+
