2b002860fae64e3f35069b01870344fad6003c31
GiovanneBYTE <giovanne_25@hotmail.com>
2022-10-03 22:20:08 -0300

    function ex

---
+++ cap3_Funcoes/ModuloPrincipal.py
@@ -0,0 +1,19 @@
+from cap3_Funcoes.indentificacaoDeFuncao import *
+
+minhaLista = []
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista, 20)
+
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+
+print("Resumindo")
+resumirValores(minhaLista)
