2e2b5cd5749a92284311de8c73de414c261eecec
GiovanneBYTE <giovanne_25@hotmail.com>
2022-10-13 21:28:57 -0300

    dicionario ex

---
+++ cap4_Dicionarios/managerUsers.py
@@ -0,0 +1,16 @@
+from cap4_Dicionarios.funcoes import *
+usuarios = {}
+
+opcao = perguntar()
+
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
+    if opcao == "I":
+        inserir(usuarios)
+    if opcao == "P":
+        pesquisar(usuarios, input("Qual o login que deseja pesquisar? "))
+    if opcao == "E":
+        excluir(usuarios, input("Qual usuario deseja excluir? "))
+    if opcao == "L":
+        listar(usuarios)
+
+    opcao = perguntar()
